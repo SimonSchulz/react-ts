517b9a8eedb02ba9c812d355a8a93fd553e40bf8
Semyon Shulga <91724012+SimonSchulz@users.noreply.github.com>
2026-04-29 21:38:11 +0300

    Update Route search parameters to include optional fields

--- src/routes/products/index.tsx
+++ src/routes/products/index.tsx
@@ -3,10 +3,14 @@ import { lazy } from 'react'
 import type { SearchParams } from '../../shared/types/searchParams.ts'
 
 export const Route = createFileRoute('/products/')<{
-  search: SearchParams
+  Search: {
+    page?: number
+    q?: string
+  }
 }>({
   validateSearch: (search) => ({
-    page: Number(search.page) > 0 ? Number(search.page) : 1
+    page: Number(search.page) > 0 ? Number(search.page) : 1,
+    q: typeof search.q === 'string' ? search.q : ''
   }),
   component: lazy(() => import('./products'))
 })
